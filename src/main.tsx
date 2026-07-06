665feb011a6d38e078df163ae364b3d5129961ad
binhusmachado-code <binhusmachado@gmail.com>
2026-07-06 18:31:19 -0400

    Phase 5: Source core files

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
+import { ToastProvider } from './components/ToastProvider.tsx'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </HashRouter>
   </React.StrictMode>,
 )
